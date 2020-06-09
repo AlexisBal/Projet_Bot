9a5908b3d130b58bb27c5df716a82f80116b1e66
mcc <60859013+AlexisBal@users.noreply.github.com>
2020-06-09 21:03:11 +0200

    master

--- Generateur_Comptes_Zalando_V2/test.py
+++ Generateur_Comptes_Zalando_V2/test.py
@@ -1,16 +1,15 @@
 import requests
+import certifi
 
 
 url_post = "https://www.zalando.fr/api/reef/register"
-url_get = "https://www.zalando.fr/login/?view=register"
 register_data = {
     'firstname': "Alexis",
     'lastname': "Balayre",
     'email': "alexis.balayre@isep.fr",
     'password': "Dubai007",
 }
-s = requests.session()
-s.get("https://www.zalando.fr/login/?view=register", verify=True)
+r = requests.post("https://www.zalando.fr/api/reef/register", data=register_data)
 
 
 #register_request = client.post(url_post, data=register_data)
