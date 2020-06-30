ac6510cb903f410140624bc84b732d3b38f23624
mcc <60859013+AlexisBal@users.noreply.github.com>
2020-06-30 12:03:15 +0200

    master

--- Verification_Stock_Produit/StockProduit.py
+++ Verification_Stock_Produit/StockProduit.py
@@ -40,75 +40,88 @@ urllib3.disable_warnings()
 def DisponibiliteProduit():
     x = 0
     while True:
-        # Ouverture de la Session
-        with requests.Session() as session:
-            # Réglage des paramètres de la session
-            session.mount("https://", TimeoutHTTPAdapter(max_retries=retries))
+        try:
+            # Ouverture de la Session
+            with requests.Session() as session:
+                # Réglage des paramètres de la session
+                session.mount("https://", TimeoutHTTPAdapter(max_retries=retries))
 
-            session.headers.update(
-                {
-                    'User-Agent': generate_user_agent(os=('mac', 'linux'))
+                session.headers.update(
+                    {
+                        'User-Agent': generate_user_agent(os=('mac', 'linux'))
+                    }
+                )
+
+                # Réglage du Proxy
+                liste_proxy = [
+                    'uk1525.nordvpn.com',
+                    'ie69.nordvpn.com',
+                    'hk139.nordvpn.com',
+                    'jp491.nordvpn.com',
+                    'au443.nordvpn.com',
+                    'pt32.nordvpn.com'
+                ]
+                print(liste_proxy[x])
+                session.proxies = {
+                    'https': 'https://alexis.balayre@gmail.com:worwaj-8kemXi-gogqes@%s:80/' % liste_proxy[x]
                 }
-            )
 
-            # Réglage du Proxy
-            liste_proxy = ['jp491.nordvpn.com', 'au443.nordvpn.com', 'pt32.nordvpn.com']
-            print(liste_proxy[x])
-            session.proxies = {
-                'https': 'https://alexis.balayre@gmail.com:worwaj-8kemXi-gogqes@%s:80/' % liste_proxy[x]
-            }
+                # Connexion à la page d'accueil de Zalando
+                url_google = 'https://www.google.com/?client=safari'
+                url_home = 'https://www.zalando.fr'
+                session.get(url_google, verify=False)
+                session.headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+                session.headers["Accept-Language"] = "fr-fr"
+                session.headers["Accept-Encoding"] = "gzip, deflate, br"
+                session.get(url_home, verify=False)
 
-            # Connexion à la page d'accueil de Zalando
-            url_google = 'https://www.google.com/?client=safari'
-            url_home = 'https://www.zalando.fr'
-            session.get(url_google, verify=False)
-            session.headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
-            session.headers["Accept-Language"] = "fr-fr"
-            session.headers["Accept-Encoding"] = "gzip, deflate, br"
-            session.get(url_home, verify=False)
+                # Récupération et modification des cookies de la session
+                cookies = session.cookies.get_dict()
+                del session.cookies['mpulseinject']
+                session.cookies['mpulseinject'] = 'false'
 
-            # Récupération et modification des cookies de la session
-            cookies = session.cookies.get_dict()
-            del session.cookies['mpulseinject']
-            session.cookies['mpulseinject'] = 'false'
+                # Connexion à la page du produit
+                url_produit = 'https://www.zalando.fr/pier-one-baskets-basses-white-pi915o00k-a11.html'
+                session.headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+                produit_session = session.get(url_produit, verify=False)
 
-            # Connexion à la page du produit
-            url_produit = 'https://www.zalando.fr/pier-one-baskets-basses-white-pi915o00k-a11.html'
-            session.headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
-            produit_session = session.get(url_produit, verify=False)
+                # Diversion anti-bot
+                url_bot = 'https://www.zalando.fr/resources/35692132da201d42d0a3ba96882c7b'
+                data_bot = {
+                    'sensor_data': '7a74G7m23Vrp0o5c9178231.59-1,2,-94,-100,Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.2 Safari/605.1.15,uaend,11011,20030107,fr-fr,Gecko,1,0,0,0,392058,5583660,1440,816,1440,900,1440,457,1440,,cpen:0,i1:0,dm:0,cwen:0,non:1,opc:0,fc:0,sc:0,wrc:1,isc:0,vib:0,bat:0,x11:0,x12:1,8824,0.310461937155,796712791830,loc:-1,2,-94,-101,do_dis,dm_dis,t_dis-1,2,-94,-105,0,0,0,0,-1,113,0;0,-1,0,0,1136,-1,0;-1,2,-94,-102,0,0,0,0,-1,113,0;0,-1,0,0,1136,-1,0;-1,2,-94,-108,-1,2,-94,-110,-1,2,-94,-117,-1,2,-94,-111,-1,2,-94,-109,-1,2,-94,-114,-1,2,-94,-103,-1,2,-94,-112,https://www.zalando.fr/reebok-classic-club-c-85-baskets-basses-whitedark-greenchalk-white-re015o075-a11.html-1,2,-94,-115,1,32,32,0,0,0,0,1,0,1593425583660,-999999,17046,0,0,2841,0,0,2,0,0,AD852DA9CB75216A40393C9EA4B27CA9~-1~YAAQLux7XEmFhfJyAQAA99qQ/wSQeuSr3yA2nUWFM355G9Au4k+JM7EsIxUEh2qBoJucheaV8nK4A7qzhqSTeRkqZmCARZtx49bK2n7Yp4WukXx0R69NrR+2q6VKAUVuGLvIi5xh3yU0oRYCSPryGSabiW3mhyrr8H+fPR8si9AU9vlhW00znwELGASR7UZzoJEB2kLR+0FBqQR2TxiK63dFJfppynpweN0GJHNXgAzWWEWBH5cAPljF1HUmYsum6zn2qMlPytR20miWUC7oUnvNMbOfYSN66+bePb5+PLjc2w/lma8ZW4PFT8UGkkZ77oa7HDRfAzPORuamSL1XvY2+QCE=~0~-1~-1,32125,-1,-1,26018161,NVFO,124,-1-1,2,-94,-106,0,0-1,2,-94,-119,-1-1,2,-94,-122,0,0,0,0,1,0,0-1,2,-94,-123,-1,2,-94,-124,-1,2,-94,-126,-1,2,-94,-127,-1,2,-94,-70,-1-1,2,-94,-80,94-1,2,-94,-116,150758334-1,2,-94,-118,88956-1,2,-94,-121,;1;-1;0'
+                }
+                session.headers["Accept"] = "*/*"
+                session.headers["Content-Type"] = "text/plain;charset=UTF-8"
+                session.headers["Origin"] = "https://www.zalando.fr"
+                session.headers["Content-Length"] = '1452'
+                session.headers["Referer"] = url_produit
+                session.post(url_bot, json=data_bot, verify=False)
 
-            # Diversion anti-bot
-            url_bot = 'https://www.zalando.fr/resources/35692132da201d42d0a3ba96882c7b'
-            data_bot = {
-                'sensor_data': '7a74G7m23Vrp0o5c9178231.59-1,2,-94,-100,Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.2 Safari/605.1.15,uaend,11011,20030107,fr-fr,Gecko,1,0,0,0,392058,5583660,1440,816,1440,900,1440,457,1440,,cpen:0,i1:0,dm:0,cwen:0,non:1,opc:0,fc:0,sc:0,wrc:1,isc:0,vib:0,bat:0,x11:0,x12:1,8824,0.310461937155,796712791830,loc:-1,2,-94,-101,do_dis,dm_dis,t_dis-1,2,-94,-105,0,0,0,0,-1,113,0;0,-1,0,0,1136,-1,0;-1,2,-94,-102,0,0,0,0,-1,113,0;0,-1,0,0,1136,-1,0;-1,2,-94,-108,-1,2,-94,-110,-1,2,-94,-117,-1,2,-94,-111,-1,2,-94,-109,-1,2,-94,-114,-1,2,-94,-103,-1,2,-94,-112,https://www.zalando.fr/reebok-classic-club-c-85-baskets-basses-whitedark-greenchalk-white-re015o075-a11.html-1,2,-94,-115,1,32,32,0,0,0,0,1,0,1593425583660,-999999,17046,0,0,2841,0,0,2,0,0,AD852DA9CB75216A40393C9EA4B27CA9~-1~YAAQLux7XEmFhfJyAQAA99qQ/wSQeuSr3yA2nUWFM355G9Au4k+JM7EsIxUEh2qBoJucheaV8nK4A7qzhqSTeRkqZmCARZtx49bK2n7Yp4WukXx0R69NrR+2q6VKAUVuGLvIi5xh3yU0oRYCSPryGSabiW3mhyrr8H+fPR8si9AU9vlhW00znwELGASR7UZzoJEB2kLR+0FBqQR2TxiK63dFJfppynpweN0GJHNXgAzWWEWBH5cAPljF1HUmYsum6zn2qMlPytR20miWUC7oUnvNMbOfYSN66+bePb5+PLjc2w/lma8ZW4PFT8UGkkZ77oa7HDRfAzPORuamSL1XvY2+QCE=~0~-1~-1,32125,-1,-1,26018161,NVFO,124,-1-1,2,-94,-106,0,0-1,2,-94,-119,-1-1,2,-94,-122,0,0,0,0,1,0,0-1,2,-94,-123,-1,2,-94,-124,-1,2,-94,-126,-1,2,-94,-127,-1,2,-94,-70,-1-1,2,-94,-80,94-1,2,-94,-116,150758334-1,2,-94,-118,88956-1,2,-94,-121,;1;-1;0'
-            }
-            session.headers["Accept"] = "*/*"
-            session.headers["Content-Type"] = "text/plain;charset=UTF-8"
-            session.headers["Origin"] = "https://www.zalando.fr"
-            session.headers["Content-Length"] = '1452'
-            session.headers["Referer"] = url_produit
-            session.post(url_bot, json=data_bot, verify=False)
+                # Accès aux informations
+                del session.headers["Content-Type"]
+                del session.headers["Content-Length"]
+                del session.headers["Origin"]
+                session.headers["x-xsrf-token"] = cookies["frsx"]
+                flowid_produit = urllib.parse.quote(produit_session.headers["X-Flow-Id"])
+                url_get_2 = (
+                        "https://www.zalando.fr/api/rr/pr/sajax?flowId=%s&try=1" % flowid_produit
+                )
+                a = session.get(url_get_2, verify=False)
 
-            # Accès aux informations
-            del session.headers["Content-Type"]
-            del session.headers["Content-Length"]
-            del session.headers["Origin"]
-            session.headers["x-xsrf-token"] = cookies["frsx"]
-            flowid_produit = urllib.parse.quote(produit_session.headers["X-Flow-Id"])
-            url_get_2 = (
-                    "https://www.zalando.fr/api/rr/pr/sajax?flowId=%s&try=1" % flowid_produit
-            )
-            a = session.get(url_get_2, verify=False)
+                # Affichage des produits disponibles
+                objet = json.loads(a.text)
+                print(objet['gtm']['productSizeAvailability'])
 
-            # Affichage des produits disponibles
-            objet = json.loads(a.text)
-            print(objet['gtm']['productSizeAvailability'])
+            session.close()
+            time.sleep(600)
+            x = x + 1
+            if x == 6:
+                x = 0
 
-        session.close()
-        time.sleep(600)
-        x = x + 1
-        if x == 3:
-            x = 0
+        except KeyError:
+            x = x + 1
+            if x == 6:
+                x = 0
 
 
 DisponibiliteProduit()
