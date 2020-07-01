5779b901f177f3b631592bdd2f35b53d7f1c7f44
mcc <60859013+AlexisBal@users.noreply.github.com>
2020-07-01 14:54:12 +0200

    master

--- Paiement_Zalando/Paiement_Zalando.py
+++ Paiement_Zalando/Paiement_Zalando.py
@@ -49,49 +49,76 @@ def creation_objet_compte():
     return compte_objet_list
 
 
-def Paiement_Zalando(liste_proxys):
-    x = 0
-    while True:
-        try:
-            # Ouverture de la Session
-            with requests.Session() as session:
-                # Réglage des paramètres de la session
-                session.mount("https://", TimeoutHTTPAdapter(max_retries=retries))
+def Paiement_Zalando(liste_proxys, compte_objet_list):
+    # Comptage du nombre de comptes présents dans la base de données
+    nombrecompte = len(compte_objet_list)
 
-                session.headers.update(
-                    {
-                        'User-Agent': generate_user_agent(os=('mac', 'linux'))
+    # Achat du produit pour chaque objet "Compte" présent dans la base de données
+    for compte in range(0, nombrecompte):
+        x = 0
+        while True:
+            try:
+                # Ouverture de la Session
+                with requests.Session() as session:
+                    # Réglage des paramètres de la session
+                    session.mount("https://", TimeoutHTTPAdapter(max_retries=retries))
+
+                    session.headers.update(
+                        {
+                            'User-Agent': generate_user_agent(os=('mac', 'linux'))
+                        }
+                    )
+
+                    # Réglage du proxy
+                    session.proxies = {
+                        'https': 'https://%s' % liste_proxys[x]
                     }
-                )
 
-                # Réglage du proxy
-                session.proxies = {
-                    'https': 'https://%s' % liste_proxys[x]
-                }
+                    # Connexion à la page d'accueil de Zalando
+                    url_google = 'https://www.google.com/?client=safari'
+                    url_home = 'https://www.zalando.fr'
+                    session.get(url_google, verify=False)
+                    session.headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+                    session.headers["Accept-Language"] = "fr-fr"
+                    session.headers["Accept-Encoding"] = "gzip, deflate, br"
+                    home = session.get(url_home, verify=False)
 
-                # Connexion à la page d'accueil de Zalando
-                url_google = 'https://www.google.com/?client=safari'
-                url_home = 'https://www.zalando.fr'
-                session.get(url_google, verify=False)
-                session.headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
-                session.headers["Accept-Language"] = "fr-fr"
-                session.headers["Accept-Encoding"] = "gzip, deflate, br"
-                session.get(url_home, verify=False)
+                    # Récupération et modification des cookies de la session
+                    cookies = session.cookies.get_dict()
 
-                # Récupération et modification des cookies de la session
-                cookies = session.cookies.get_dict()
+                    # Connexion à la page de connexion
+                    url_connexion_1 = "https://www.zalando.fr/login/?view=login"
+                    session.get(url_connexion_1, verify=False)
 
-            # Fermeture de la Session
-            session.close()
+                    # Connexion au compte
+                    url_connexion_2 = 'https://www.zalando.fr/api/reef/login/schema'
+                    url_connexion_3 = 'https://www.zalando.fr/api/reef/login'
+                    identifiants = {
+                        "username": compte_objet_list[compte].email,
+                        "password": compte_objet_list[compte].motdepasse,
+                        "wnaMode": "shop"
+                    }
+                    session.headers["x-xsrf-token"] = cookies["frsx"]
+                    session.headers["x-zalando-client-id"] = cookies["Zalando-Client-Id"]
+                    session.headers["x-zalando-render-page-uri"] = "/login/?view=login"
+                    session.headers["x-zalando-request-uri"] = "/login/?view=login"
+                    session.headers["x-flow-id"] = home.headers["X-Flow-Id"]
+                    session.headers["Accept"] = "application/json"
+                    session.get(url_connexion_2, verify=False)
+                    session.headers["Origin"] = "https://www.zalando.fr"
+                    session.post(url_connexion_3, json=identifiants, verify=False)
 
-        # Gestion des exceptions
-        except:
-            pass
+                # Fermeture de la Session
+                session.close()
 
-        finally:
-            x = x + 1
-            if x == (len(liste_proxys) + 1):
-                x = 0
+            # Gestion des exceptions
+            except:
+                pass
+
+            finally:
+                x = x + 1
+                if x == (len(liste_proxys) + 1):
+                    x = 0
 
 
 url_1 = 'https://www.zalando.fr/cart'
@@ -99,3 +126,6 @@ url_2 = 'https://www.zalando.fr/checkout/address'
 url_2_bis = 'https://www.zalando.fr/api/checkout/search-pickup-points-by-address'
 url_3 = 'https://www.zalando.fr/checkout/confirm'
 
+
+comptes = creation_objet_compte()
+Paiement_Zalando(comptes)
