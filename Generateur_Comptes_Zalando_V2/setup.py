ceecb9da66e526b57a4ffe13caa7c77d8786350e
mcc <60859013+AlexisBal@users.noreply.github.com>
2020-07-04 17:51:08 +0200

    master

--- Generateur_Comptes_Zalando_V2/setup.py
+++ Generateur_Comptes_Zalando_V2/setup.py
@@ -3,8 +3,7 @@ from cx_Freeze import setup, Executable
 # Dependencies are automatically detected, but it might need
 # fine tuning.
 buildOptions = dict(
-    packages=["idna", "json", "time", "requests", "password_generator", "urllib3", "requests.adapters", "requests.packages.urllib3.util.retry"]
-    include_files=["Comptes.json"],
+    packages=["idna", "json", "requests", "password_generator", "urllib3", "requests.adapters", "requests.packages.urllib3.util.retry",  'user_agent']
 )
 
 base = None
