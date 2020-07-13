ba065e3079b439c5060bc7b018fee12222b6fe02
mcc <60859013+AlexisBal@users.noreply.github.com>
2020-07-13 15:36:58 +0200

    master

--- Bot_Zalando_3.0/test.py
+++ Bot_Zalando_3.0/test.py
@@ -1,17 +1,3 @@
-# Création de la liste de compte "Liste_compte2"
-def compte2():
-    with open('../Data/Accounts_List2.csv', 'r') as f:
-        Liste_compte2 = []
-        for ligne in f:
-            compte_list2 = ligne.split(";")
-            Liste_compte2.append(compte_list2)
-    f.close()
-    return Liste_compte2
+from termcolor import colored
 
-
-def test(compte):
-    test = compte[1][6] + " " + compte[1][7]
-    print(test)
-
-compte = compte2()
-test(compte)
+print(colored('hello', 'red'), colored('world', 'green'))
