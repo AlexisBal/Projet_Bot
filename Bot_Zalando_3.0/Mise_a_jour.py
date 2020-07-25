7aee8f5e107d23b281294c6aa6cfb144c1f1769c
Richard Kienitz <kienitzrichard@yahoo.fr>
2020-07-25 13:04:44 +0200

    mise à jour

--- Bot_Zalando_3.0/Mise_a_jour.py
+++ Bot_Zalando_3.0/Mise_a_jour.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 import requests
 from tkinter import messagebox
 
@@ -5,15 +6,29 @@ version=1.0
 Nom_application='ScredAIO'
 
 def mise_a_jour():
-    
-    response = requests.get(
-    'https://raw.githubusercontent.com/AlexisBal/Projet_Bot/master/Bot_Zalando_3.0/Data/Version.txt?token=AO2H2FQPIOVTLAPT3SBGF6K7DQESE')
-    data = response.text
-    print(data)
+    def check_updates():
+            try:
+                response = requests.get(
+                    'https://raw.githubusercontent.com/AlexisBal/Projet_Bot/master/Bot_Zalando_3.0/Data/Version.txt?token=AO2H2FX6SIFTLKLFHGFACQK7DQIAC')
+                data = response.text
 
-    if float(data) > float(version):
+                if float(data) > float(__version__):
                     messagebox.showinfo('Software Update', 'Update Available!')
-                    mb1 = messagebox.askyesno('Update!', f'{Nom_application} {version} needs to update to version {data}')
+                    message = messagebox.askyesno('Update !', f'{Nom_application} {version} needs to update to version {data}')
+                    if message is True:
+
+                       
+                        webbrowser.open_new_tab('')
+                        parent.destroy()
+
+
+
+                    else:
+                        pass
+                else:
+                    messagebox.showinfo('Software Update', 'No Updates are Available.')
+            except Exception as e:
+                messagebox.showinfo('Software Update', 'Unable to Check for Update, Error:' + str(e))  
 
 
 
