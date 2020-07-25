d7e05df3e0a4c4e728c918ed160b44f03f476cd8
Richard Kienitz <kienitzrichard@yahoo.fr>
2020-07-25 12:29:23 +0200

    Check update

--- Bot_Zalando_3.0/Mise_a_jour.py
+++ Bot_Zalando_3.0/Mise_a_jour.py
@@ -1,11 +1,20 @@
 import requests
+from tkinter import messagebox
+
+version=1.0
+Nom_application='ScredAIO'
 
 def mise_a_jour():
     
     response = requests.get(
-    'https://raw.githubusercontent.com/AlexisBal/Projet_Bot/master/Bot_Zalando_3.0/Data/Version.txt?token=AO2H2FXAKZO47FPUA7FM23S7DQDXA')
+    'https://raw.githubusercontent.com/AlexisBal/Projet_Bot/master/Bot_Zalando_3.0/Data/Version.txt?token=AO2H2FQPIOVTLAPT3SBGF6K7DQESE')
     data = response.text
     print(data)
+
+    if float(data) > float(version):
+                    messagebox.showinfo('Software Update', 'Update Available!')
+                    mb1 = messagebox.askyesno('Update!', f'{Nom_application} {version} needs to update to version {data}')
+
 
 
 
