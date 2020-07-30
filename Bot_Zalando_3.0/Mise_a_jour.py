e457d8a7a4c098659b39f8828693cdea76bde5e9
Richard Kienitz <kienitzrichard@yahoo.fr>
2020-07-30 02:44:18 +0200

    jour

--- Bot_Zalando_3.0/Mise_a_jour.py
+++ Bot_Zalando_3.0/Mise_a_jour.py
@@ -1,11 +1,10 @@
 import tkinter as tk
 import requests
-from tkinter import messagebox
-from bs4 import BeautifulSoup
+from tkinter import messagebox, ttk
 import webbrowser
 
 version=1.0
-Nom_application='ScredAIO'
+Nom_application='Bot'
 
 def mise_a_jour():
     
@@ -13,33 +12,40 @@ def mise_a_jour():
         response = requests.get(
             'https://raw.githubusercontent.com/JamesBond65/Hujub/master/Rnadom%201/Random/Version.txt')
         data = response.text
+        
 
         if float(data) > float(version):
             messagebox.showinfo('Software Update', 'Update Available!')
             message = messagebox.askyesno('Update !', f'{Nom_application} {version} needs to update to version {data}')
             if message is True:
                        
-                webbrowser.open_new_tab('https://raw.githubusercontent.com/JamesBond65/Hujub/master/Rnadom%201/Random/Version.txt')
-                
-                parent.destroy()
+                response = requests.get('https://raw.githubusercontent.com/JamesBond65/Hujub/master/Rnadom%201/Principale.py')
+                code = response.text
+                print(code)
 
+                with open("Bot_Zalando_3.0/ScredAIO1.py", "w") as f:
+                    f.write(code)
+                
+                pass
 
 
             else:
                 pass
         else:
             messagebox.showinfo('Software Update', 'No Updates are Available.')
+            
     except Exception as e:
-        messagebox.showinfo('Software Update', 'Unable to Check for Update, Error:' + str(e))  
+        messagebox.showinfo('Software Update', 'Unable to Check for Update, Error:' + str(e))
+
+
+
+
+mise_a_jour()
 
 
 
 
 #mise_a_jour()
-
-#with requests.Session() as session:
-
-
 
 #response = requests.get('https://raw.githubusercontent.com/JamesBond65/Hujub/master/Rnadom%201/Random/Version.txt')
 #print(response.text)
