26fac2de2077e50fb8d48bf69eadc193984c19b8
Richard Kienitz <kienitzrichard@yahoo.fr>
2020-08-01 01:18:39 +0200

    ca fonctionne

--- Bot_Zalando_3.0/Mise_a_jour.py
+++ Bot_Zalando_3.0/Mise_a_jour.py
@@ -8,7 +8,7 @@ import urllib.request as ur
 
 
 
-def mise_a_jour(version):
+def mise_a_jour():
     version=1.0
     Nom_application='ScredAIO'
     try:
@@ -23,13 +23,22 @@ def mise_a_jour(version):
             message = messagebox.askyesno('Update !', f'{Nom_application} {version} needs to update to version {data}')
             if message is True:
                        
-
-                url = 'https://raw.github.com/JamesBond65/Hujub/master/Random.1/dist/update.exe'
+                
+                
                 directory = getcwd()
                 print(directory)
                 filename = directory + '\Bot_Zalando_3.0\ScredAIO.exe'
                 print(filename)
-                r = requests.get(url, allow_redirects=True)
+                
+                headers = {
+                    'Authorization': 'token 166c3f0372a93dd3c1f9e6358e5b39bfb8b57d76',
+                    'Accept': 'application/vnd.github.v3.raw',
+                        }
+
+                r = requests.get(
+                    'https://raw.githubusercontent.com/AlexisBal/Projet_Bot/master/Bot_Zalando_3.0/Test_telechargement_du_.exe/update.exe', 
+                    headers=headers, allow_redirects=True)
+                
 
                 f = open(filename,'wb')
                 f.write(r.content)
