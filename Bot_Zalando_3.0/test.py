27760fded144df905eedb60b6cd4c778f2f70212
mcc <60859013+AlexisBal@users.noreply.github.com>
2020-08-03 10:35:00 +0200

    master

--- Bot_Zalando_3.0/test.py
+++ Bot_Zalando_3.0/test.py
@@ -6,24 +6,6 @@ from user_agent import generate_user_agent
 import random
 
 
-# Réglage des "Timeouts"
-class TimeoutHTTPAdapter(HTTPAdapter):
-    def __init__(self, *args, **kwargs):
-        self.timeout = 8
-        if "timeout" in kwargs:
-            self.timeout = kwargs["timeout"]
-            del kwargs["timeout"]
-        super().__init__(*args, **kwargs)
-
-    def send(self, request, **kwargs):
-        timeout = kwargs.get("timeout")
-        if timeout is None:
-            kwargs["timeout"] = self.timeout
-        return super().send(request, **kwargs)
-
-
-# Réglage des "Retries"
-retries = Retry(total=3, backoff_factor=0, status_forcelist=[429, 500, 502, 503, 504])
 urllib3.disable_warnings()
 headers = {
     "User-Agent": generate_user_agent()
