36c23f28e250478fbbbeee29b2430924643ddd37
Richard Kienitz <kienitzrichard@yahoo.fr>
2020-08-10 13:50:12 +0200

    .

--- Bot_Zalando_3.0/generateur_mail.py
+++ Bot_Zalando_3.0/generateur_mail.py
@@ -1,8 +1,24 @@
 from guerrillamail import GuerrillaMailSession
+import time
+import requests
 
 
-session = GuerrillaMailSession()
+'''session = GuerrillaMailSession()
 print (session.get_session_state()['email_address'])
 print( session.get_email_list()[0].guid)
 print(session.get_email(1))
-#print(session.set_email_address(session)
+time.sleep(30)
+print( session.get_email_list()[0].guid)
+
+print()
+#print(session.set_email_address(session)'''
+
+r= requests.get('https://temp-mail.org/fr/')
+print(r.cookies)
+print("\n\n")
+
+session = requests.Session()
+print(session.cookies.get_dict())
+
+response = session.get('https://temp-mail.org/fr/')
+print(session.cookies.get_dict())
